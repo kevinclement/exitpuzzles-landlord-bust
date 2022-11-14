f407101a224f287923806e661da74dbab6c4160d
Kevin Clement <kevin.clement@gmail.com>
2022-11-14 10:58:38 -0800

    main logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+// How this works:
+//   I used to use a digital out to control stuff.  However, with the current wiring, its not needed
+//   All that I rely on is sending 3v signal to the input of the other microcontroller.  This will
+//   happen as soon as this device gets powered on, so there is no need to have any logic at all.
+//
+//   BUST -> Switches 120v ON -> Powers on 5v Adapter to power Arduino -> Sends 3v out is 3v line -> Triggers logic in main
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Bust-opener by kevinc");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
